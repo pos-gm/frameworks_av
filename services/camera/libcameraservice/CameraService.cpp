7140829f8bec1aafb4f2238142744b6f88f72376
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-03-15 17:42:18 +0545

    Revert "libcameraservice: Add support to set vendor tag with client package name"
    This reverts commit 603f89db59a863b3e68dbd5a0be209d01a668b29.
    
    Change-Id: Ieee7ba9e998fc9623f9aa4b6367fa7a4c9366169

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -185,9 +185,6 @@ constexpr int32_t kInvalidDeviceId = -1;
 // Set to keep track of logged service error events.
 static std::set<std::string> sServiceErrorEventSet;
 
-// Current camera package name
-static std::string sCurrPackageName;
-
 CameraService::CameraService(
         std::shared_ptr<CameraServiceProxyWrapper> cameraServiceProxyWrapper,
         std::shared_ptr<AttributionAndPermissionUtils> attributionAndPermissionUtils) :
@@ -1488,10 +1485,6 @@ Status CameraService::filterGetInfoErrorCode(status_t err) {
     }
 }
 
-std::string CameraService::getCurrPackageName() {
-    return sCurrPackageName;
-}
-
 Status CameraService::makeClient(
         const sp<CameraService>& cameraService, const sp<IInterface>& cameraCb,
         const AttributionSourceState& clientAttribution, int callingPid, bool systemNativeClient,
@@ -2485,8 +2478,6 @@ Status CameraService::connectHelper(const sp<CALLBACK>& cameraCb, const std::str
                                     bool sharedMode, bool isVendorClient,
                                     /*out*/ sp<CLIENT>& device) {
     binder::Status ret = binder::Status::ok();
-
-    sCurrPackageName = clientPackageName;
 
     nsecs_t openTimeNs = systemTime();
 
